ad5334273d0e3b4d55a256e6ef306008d116eded
Kshitijkumar16 <85787820+Kshitijkumar16@users.noreply.github.com>
2024-02-04 13:42:59 +0530

    correcting checkout routes

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -64,8 +64,8 @@ export async function POST(
 		mode: "payment",
 		billing_address_collection: "required",
 		phone_number_collection: { enabled: true },
-		success_url: `${process.env.FRONTEND_STORE_URL}/cart/success=1`,
-		cancel_url: `${process.env.FRONTEND_STORE_URL}/cart/canceled=1`,
+		success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+		cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
 		metadata: { orderId: order.id },
 	});
 
